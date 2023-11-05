4df2d8924cf860fd5c9b593199a91fa31cd37ad0
Davidq-Desktop <dv.querol@gmail.com>
2023-11-05 22:06:38 +0100

    added interface Statea

--- src/components/MyReactComponent.tsx
+++ src/components/MyReactComponent.tsx
@@ -1,4 +1,63 @@
+import type { State } from './types.d'
+
+// 1. Create a initialState
+const initialState: State = {
+    fromLanguage: 'auto',
+    toLanguage: 'en',
+    fromText: '',
+    result: '',
+    loading: false
+}
+
+// 2. Create a reducer
+function reducer(state: State, action) {
+    const { type, payload } = action
+
+    if (type === 'INTERCHANGE_LANGUAGES') {
+        return {
+            ...state,
+            fromLanguage: state.toLanguage,
+            toLanguage: state.fromLanguage
+        }
+    }
+
+    if (type === 'SET_FROM_LANGUAGE') {
+        return {
+            ...state,
+            fromLanguage: payload
+        }
+    }
+
+    if (type === 'SET_TO_LANGUAGE') {
+        return {
+            ...state,
+            toLanguage: payload
+        }
+    }
+
+    if (type === 'SET_FROM_TEXT') {
+        return {
+            ...state,
+            loading: true,
+            fromText: payload,
+            result: ''
+        }
+    }
+
+    if (type === 'SET_RESULT') {
+        return {
+            ...state,
+            loading: false,
+            result: payload
+        }
+    }
+
+    return state
+}
+
 function App() {
+    // 3. Usar el hook reducer
+    const [state, dispatch] = useReducer(reducer, initialState)
     return (
         <div className="App">
             <h1>Google Translate</h1>
