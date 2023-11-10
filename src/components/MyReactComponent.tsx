478a1478443aa1e51352279b8cc1654bbbc18e9a
Davidq-Desktop <dv.querol@gmail.com>
2023-11-10 18:01:55 +0100

    hooks created

--- src/components/MyReactComponent.tsx
+++ src/components/MyReactComponent.tsx
@@ -1,72 +1,13 @@
-import { useReducer } from 'react'
-import type { State, Action } from './types.d'
-
-// 1. Create a initialState https://docs.astro.build/en/core-concepts/framework-components/#using-framework-components
-const initialState: State = {
-    fromLanguage: 'auto',
-    toLanguage: 'en',
-    fromText: '',
-    result: '',
-    loading: false
-}
-
-// 2. Create a reducer https://docs.astro.build/en/guides/typescript/
-function reducer(state: State, action: Action) {
-    const { type } = action
-
-    if (type === 'INTERCHANGE_LANGUAGES') {
-        return {
-            ...state,
-            fromLanguage: state.toLanguage,
-            toLanguage: state.fromLanguage
-        }
-    }
-
-    if (type === 'SET_FROM_LANGUAGE') {
-        return {
-            ...state,
-            fromLanguage: action.payload
-        }
-    }
-
-    if (type === 'SET_TO_LANGUAGE') {
-        return {
-            ...state,
-            toLanguage: action.payload
-        }
-    }
-
-    if (type === 'SET_FROM_TEXT') {
-        return {
-            ...state,
-            loading: true,
-            fromText: action.payload,
-            result: ''
-        }
-    }
-
-    if (type === 'SET_RESULT') {
-        return {
-            ...state,
-            loading: false,
-            result: action.payload
-        }
-    }
-
-    return state
-}
+import { useStore } from '../hooks/useStore'
 
 function App() {
-    // 3. Usar el hook reducer
-    const [{ fromLanguage, toLanguage, fromText, result, loading }, dispatch] = useReducer(reducer, initialState)
+    const { fromLanguage, setFromLanguage } = useStore();
 
     return (
         <div className="App">
             <h1>Google Translate</h1>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                onClick={() => {
-                    dispatch({ type: 'SET_FROM_LANGUAGE', payload: 'es' })
-                }}>
+                onClick={() => { setFromLanguage('es') }}>
                 Cambiar a Español
             </button>
             {fromLanguage}
