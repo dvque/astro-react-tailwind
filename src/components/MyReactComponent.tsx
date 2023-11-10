40ee15b24bbfe09c474ad7db9e01addfb49f40e3
Davidq-Desktop <dv.querol@gmail.com>
2023-11-10 19:10:27 +0100

    Add language selection and translation functionality to MyReactComponent.tsx and import Font Awesome icon library in Layout.astro

--- src/components/MyReactComponent.tsx
+++ src/components/MyReactComponent.tsx
@@ -14,46 +14,45 @@ function App() {
                     </div>
                 </div>
             </div>
-            <div className="flex mb-4">
-                <div className="w-1/2 bg-gray-400 h-12"></div>
-                <div className="w-1/2 bg-gray-500 h-12"></div>
-            </div>
-            <div className="flex mb-4">
-                <div className="w-full bg-gray-500 h-12"></div>
-            </div>
 
-            <h1>Google Translate</h1>
+            <form className="flex items-center justify-center mx-auto">
+                <div className="w-1/3">
+                    <select className="form-select block w-full border border-gray-200 rounded py-3 px-4">
+                        <option>Option 1</option>
+                        <option>Option 2</option>
+                    </select>
+                    
+                </div>
+                <div className="mx-6">
+                    <i className="fas fa-exchange-alt"></i>
+                </div>
+                <div className="w-1/3">
+                    <select className="form-select block w-full border border-gray-200 rounded py-3 pr-2 pl-4">
+                        <option>Option 1</option>
+                        <option>Option 2</option>
+                    </select>
+                </div>
+            </form>
+
+            <div className="flex items-center justify-center mx-auto">
+                <div className="w-1/3">
+                {fromLanguage}
+                    
+                </div>
+                <div className="mx-6">
+                 
+                </div>
+                <div className="w-1/3">
+                    
+                </div>
+            </div>
+           
+
+
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
                 onClick={() => { setFromLanguage('es') }}>
                 Cambiar a Español
             </button>
-            {fromLanguage}
-            <form className='w-full max-w-lg'>
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                            First Name
-                        </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-                    </div>
-                    <div className="w-full md:w-1/2 px-3">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                            Last Name
-                        </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
-                    </div>
-                    <div className="relative">
-                        <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                            <option>New Mexico</option>
-                            <option>Missouri</option>
-                            <option>Texas</option>
-                        </select>
-                        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-                        </div>
-                    </div>
-                </div>
-            </form>
         </div>
     )
 }
