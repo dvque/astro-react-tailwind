4c7748c6580fc85dc0ffdd1fd4ded0f0a25e76d2
Davidq-Desktop <dv.querol@gmail.com>
2023-11-14 21:03:38 +0100

    Refactor LanguageSelector component to use type aliases and enum

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,10 +1,10 @@
 
 import { AUTO_LANGUAGE, SUPPORTED_LANGUAGES } from "../hooks/constants"
-import type { FromLanguage, Language } from "./types"
+import { type FromLanguage, type Language, SectionType } from "./types"
 
 type Props =
-    | { type: 'from', value: FromLanguage, onChange: (language: FromLanguage) => void }
-    | { type: 'to', value: Language, onChange: (language: Language) => void }
+    | { type: SectionType.From, value: FromLanguage, onChange: (language: FromLanguage) => void }
+    | { type: SectionType.To, value: Language, onChange: (language: Language) => void }
 
 export const LanguageSelector = ({ onChange, type, value }: Props) => {
     const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -15,7 +15,7 @@ export const LanguageSelector = ({ onChange, type, value }: Props) => {
         <form aria-aria-label='Selecciona el idioma' className="w-1/3">
             <select onChange={handleChange} value={value} className="form-select block w-full border border-gray-200 rounded py-3 px-4">
 
-                {type === 'from' && <option value={AUTO_LANGUAGE}>Detect language</option>}
+                {type === SectionType.From && <option value={AUTO_LANGUAGE}>Detect language</option>}
 
                 {Object.entries(SUPPORTED_LANGUAGES).map(([key, value]) => (
                     <option key={key} value={key}>
