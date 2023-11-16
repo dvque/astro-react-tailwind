0206605ed77b13a53444851636a410250694182c
Davidq-Desktop <dv.querol@gmail.com>
2023-11-16 19:11:52 +0100

    Update MyReactComponent.tsx with new UI design

--- src/components/MyReactComponent.tsx
+++ src/components/MyReactComponent.tsx
@@ -8,21 +8,30 @@ function App() {
 
     return (
         <div className="App">
-            <div className="bg-white">
-                <div className="mx-auto max-w-7xl py-24 sm:px-2 sm:py-10 lg:px-8">
-                    <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-                        <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-8 lg:text-left">
-                            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Translate</h2>
-                        </div>
+
+            <div className="bg-white py-12 ">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto max-w-2xl lg:text-center">
+                        <h2 className="text-base font-semibold leading-7 text-indigo-600">ChatGPT Traductor</h2>
+                        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                            Everything you need to deploy your app
+                        </p>
+                        <p className="mt-6 text-lg leading-8 text-gray-600">
+                            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
+                            pulvinar et feugiat blandit at. In mi viverra elit nunc.
+                        </p>
                     </div>
                 </div>
             </div>
 
+
+
             <div className="flex items-center justify-center mx-auto">
+
                 <LanguageSelector type={SectionType.From} value={fromLanguage} onChange={setFromLanguage} />
 
                 <div className="mx-6">
-                    <button className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}>
+                    <button className="rounded-md  px-3.5 py-2.5 text-sm font-semibold t hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}>
                         <i className="fas fa-exchange-alt"></i>
                     </button>
                 </div>
@@ -30,17 +39,26 @@ function App() {
                 <LanguageSelector type={SectionType.To} value={toLanguage} onChange={setToLanguage} />
             </div>
 
-            <div className="flex items-center justify-center mx-auto">
-                <div> from: {fromLanguage}
-                    to: {toLanguage}</div>
+            <div className="flex flex-row mt-5">
+                <div className="basis-1/2">
+                    <textarea
+                        id="about"
+                        name="about"
+                        rows={3}
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder='Introducir texto' autoFocus
+                        defaultValue={''}
+                    /></div>
+                <div className="basis-1/2">
+                    <textarea
+                        id="about"
+                        name="about"
+                        rows={3}
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        defaultValue={''}
+                    /></div>
             </div>
-
         </div>
-
     )
-
-
-
 }
 
 export default App
