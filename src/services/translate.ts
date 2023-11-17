8783ce95e8ae0278a215f76832b764e5bd237b32
Davidq-Desktop <dv.querol@gmail.com>
2023-11-17 18:17:07 +0100

    Add language translation functionality to translate.ts

--- src/services/translate.ts
+++ src/services/translate.ts
@@ -12,10 +12,53 @@ export async function translate({ fromLanguage, toLanguage, text }: {
     toLanguage: Language,
     text: string
 }) {
-    const message = [
+
+    if (fromLanguage === toLanguage) return text;
+
+    const messages = [
         {
             role: 'system',
             content: 'You are a AI that trasnlates text. You receive a text from the user. Do not answer, just translate the text. The original language is surrounded by `{{` and `{{`. You can also receive {{auto}} which means that toy have to detect the language. The language you translate to is surrounded by `[[` and `]]`.'
+        },
+        {
+            role: 'user',
+            content: `Hola mundo {{Español}} [[English]]`
+        },
+        {
+            role: 'assistant',
+            content: `Hello world`
+        },
+        {
+            role: 'user',
+            content: `How are you? {{auto}} [[Deutsch]]`
+        },
+        {
+            role: 'assistant',
+            content: `Wie geht es dir?`
+        },
+        {
+            role: 'user',
+            content: `Bon dia, com estas? {{auto}} [[Español]]`
+        },
+        {
+            role: 'assistant',
+            content: `Buenos días, ¿cómo estás?`
         }
     ]
+
+    const fromCode = fromLanguage === 'auto' ? 'auto' : SUPPORTED_LANGUAGES[fromLanguage];
+    const toCode = SUPPORTED_LANGUAGES[toLanguage];
+
+    const completion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+
+            {
+                role: 'user',
+                content: `${text} {{${fromCode}}} [[${toCode}]}`
+            }
+        ]
+    })
+
+    return completion.choices[0]?.message?.content
 }
