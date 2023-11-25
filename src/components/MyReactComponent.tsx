c51cec268a1c7c82741e2bbd67b179c803408811
dvque <dvdquerol@gmail.com>
2023-11-25 20:31:05 +0100

    Refactor UI layout in MyReactComponent and index.astro

--- src/components/MyReactComponent.tsx
+++ src/components/MyReactComponent.tsx
@@ -44,21 +44,8 @@ function App() {
     }
 
     return (
-        <div className="App">
+        <div>
 
-            <div className="bg-white py-12 ">
-                <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                    <div className="mx-auto max-w-2xl lg:text-center">
-                        <h2 className="text-base font-semibold leading-7 text-indigo-600">Language Translation</h2>
-                        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                            ChatGPT Traductor
-                        </p>
-                        <p className="mt-6 text-lg leading-8 text-gray-600">
-                            Harnessing the power of advanced AI technology to break down language barriers, enhance cross-cultural communication, and create a more interconnected global community through seamless language translation.
-                        </p>
-                    </div>
-                </div>
-            </div>
 
             <div className="flex items-center justify-center mx-auto">
 
